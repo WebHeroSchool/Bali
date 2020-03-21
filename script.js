ced1ab7bf7b5ae4d67f3a7790e927c551f289ff1
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-03-22 01:08:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let klick = document.querySelector('.header__menu-icon');
-
-klick.onclick = function() {
-  klick.classList.toggle('menu_state_open'); 
-}
+(function($){
+  $(function() {
+    $('.header__menu-icon').on('click', function() {
+      $(this).closest('.header').toggleClass('menu_state_open');
+    });
+  });
+})(jQuery);
